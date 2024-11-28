33b690c5389101e53ff7f7dc9eff0a724aafe7db
Grazielle Café <graziellecafe@aluno.ufop.edu.br>
2024-11-28 10:43:28 -0300

    feat: change frontend styles, adding background image

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,21 @@
 import React from "react";
 import "./App.css";
+import zaptLogo from "./images/zapt-logo.svg";
 
 function App() {
   return (
     <div className="container">
-      {/* Cabeçalho */}
       <header className="header">
-        <h1>Mapa Zapt Tech</h1>
+        <div className="logo">
+          <img src={zaptLogo} alt="Zapt Tech Logo" style={{ height: "50px" }} />
+        </div>
+        <h1>Zapt Tech</h1>
       </header>
-      {/* Conteúdo principal */}
       <div className="content">
         <div className="map-container">
           <iframe
             src="https://app.zapt.tech/#/map?placeId=-ltvysf4acgzdxdhf81y&embed=true"
-            title="Mapa Zapt Tech"
+            title="Zapt Tech"
             style={{
               width: "90%",
               height: "90%",
@@ -26,7 +28,6 @@ function App() {
           <p>Adicione informações ou uma lista de lojas aqui.</p>
         </div>
       </div>
-      {/* Rodapé */}
       <footer className="footer">
         <h2>Zapt Tech</h2>
         <p>&copy; 2024 Zapt Tech</p>
