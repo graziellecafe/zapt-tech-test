34f7093146c521bd39064383bfaaa2b3dccb637d
Grazielle Café <graziellecafe@aluno.ufop.edu.br>
2024-12-04 15:17:25 -0300

    fix: fix backent showing the store list on terminal

--- backend/src/config/apiConfig.js
+++ backend/src/config/apiConfig.js
@@ -1,13 +1,12 @@
-require("dotenv").config();
 const axios = require("axios");
 
 const api = axios.create({
-  baseURL: "https://api.zapt.tech/v2/interests",
+  baseURL: "https://api.zapt.tech/v2/interests", // URL da API que você está utilizando
   headers: {
-    "x-api-key": process.env.API_KEY,
+    "x-api-key": "26ee8805-55f8-484a-a229-59d813131484", // Chave da API
   },
   params: {
-    placeId: "-ltvysf4acgzdxdhf81y",
+    placeId: "-ltvysf4acgzdxdhf81y", // Adapte conforme necessário
   },
 });
 
